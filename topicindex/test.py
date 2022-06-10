6704a25583fc6c7cbf0f1cbfc536855b5f469fe7
ag143 <ag14341@gmail.com>
2022-06-10 23:07:12 +0530

    Update test.py

--- topicindex/test.py
+++ topicindex/test.py
@@ -3,8 +3,9 @@ import json
 import os
 print(os.getcwd())
 dir = os.path.dirname('README.md')
-file = os.path.join(dir, 'Json/topcindex/git.csv')
-json_file = os.path.join(dir, './Json/topcindex/test_output.json')
+
+file = os.path.join(dir, '/topcindex/git.csv')
+json_file = os.path.join(dir, '/topcindex/test_output.json')
 
 #Read CSV File
 def read_CSV(file, json_file):
